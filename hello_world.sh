df1ac8ebcb7f95334983f1107ae657263a0945f2
Stephanie Douglas <stephanietdouglas@gmail.com>
2024-06-19 14:49:06 -0500

    update hello world for new cluster

--- hello_world.sh
+++ hello_world.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #
-#SBATCH --job-name=douglste_hello_world
+#SBATCH --job-name=hello_world
 #SBATCH --output=sample_output.txt
+#SBATCH --account=douglste-laf-lab
+#SBATCH --partition=douglste-laf-lab, compute, unowned
 #
 #SBATCH --ntasks=1
 #SBATCH --time=3:00
 #SBATCH --mem-per-cpu=100mb
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=douglste@lafayette.edu
+#SBATCH --mail-user=
 
 echo "hello world"
 srun sleep 60
-echo hostname
+srun hostname
